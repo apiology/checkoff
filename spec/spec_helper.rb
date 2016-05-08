b266b11ea0ca900f1af86118c92ff61d430d54d3
Vince Broz <vince@broz.cc>
2016-05-08 12:32:45 -0400

    Support testing executables

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,25 @@ RSpec.configure do |config|
   end
 end
 
+# Monkeypatch RSpec to help us find our place
+module RSpec
+
+  module_function
+
+  def root
+    @rspec_root ||= Pathname.new(__dir__)
+  end
+end
+
+# Add the exe directory, to allow testing of gem executables as if the gem is
+# already installed.
+exec_dir = RSpec.root.join("../exe")
+ENV['PATH'] = [ exec_dir, ENV['PATH'] ].join(File::PATH_SEPARATOR)
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[RSpec.root.join("support/**/*.rb")].each { |f| require f }
+
 def let_double(*doubles)
   doubles.each do |double_sym|
     let(double_sym) { double(double_sym.to_s) }
