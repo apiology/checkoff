1ee10065265af3bb7222ad8bf78f23cce6dba27d
Bundle updater <circleci@broz.cc>
2017-12-30 16:14:50 -0500

    More coverage

--- test/unit/test_tasks.rb
+++ test/unit/test_tasks.rb
@@ -5,6 +5,10 @@ require 'checkoff/cli'
 
 # Test the Checkoff::Tasks class
 class TestTasks < BaseAsana
+  extend Forwardable
+
+  def_delegators :@mocks, :sections
+
   let_mock :mock_tasks, :modified_mock_tasks, :tasks_by_section,
            :unflattened_modified_mock_tasks
 
@@ -46,6 +50,38 @@ class TestTasks < BaseAsana
       mock_add_task
     end
     tasks.add_task(task_name, workspace_id: workspace_id)
+  end
+
+  let_mock :workspace_name, :project_name, :task_name, :only_uncompleted, :task,
+           :projects, :project
+
+  def expect_tasks_from_project_pulled
+    projects.expects(:tasks_from_project)
+      .with(project, only_uncompleted: only_uncompleted)
+      .returns([task])
+    task.expects(:name).returns(task_name)
+  end
+
+  def expect_project_pulled
+    projects.expects(:project).with(workspace_name, project_name)
+      .returns(project)
+  end
+
+  def expect_projects_pulled
+    sections.expects(:projects).returns(projects)
+  end
+
+  def mock_task
+    expect_projects_pulled
+    expect_project_pulled
+    expect_tasks_from_project_pulled
+  end
+
+  def test_task
+    tasks = get_test_object { mock_task }
+    returned_task = tasks.task(workspace_name, project_name, task_name,
+                               only_uncompleted: only_uncompleted)
+    assert_equal(task, returned_task)
   end
 
   def class_under_test
