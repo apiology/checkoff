97e5bd3eec47e54e578ec38be354a99f9ef98726
Vince Broz <vince@broz.cc>
2021-02-04 13:33:57 -0500

    Add unit test for ConfigLoader.rb (#23)

--- lib/checkoff/config_loader.rb
+++ lib/checkoff/config_loader.rb
@@ -6,10 +6,14 @@ require 'active_support/core_ext/hash'
 module Checkoff
   # Load configuration file
   class ConfigLoader
-    def self.load(sym)
+    def self.load_yaml_file(sym)
       file = "#{sym}.yml"
       YAML.load_file(File.expand_path("~/.#{file}"))
           .with_indifferent_access
+    end
+
+    def self.load(sym)
+      load_yaml_file(sym)
     end
   end
 end
