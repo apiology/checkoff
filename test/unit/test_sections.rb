1db7abd756d3f0456d331e041ba23cb50e5774c6
Vince Broz <vince@broz.cc>
2021-02-22 16:36:49 -0500

    Add special case to handle :my_tasks in the face of API transitions (#27)
    * Add special case to handle :my_tasks in the face of API transitions
    
    * Fix test coverage
    
    * Allow bigfiles slip
    
    * Allow overcommit slip

--- test/unit/test_sections.rb
+++ test/unit/test_sections.rb
@@ -7,6 +7,8 @@ require 'active_support/time'
 
 # Test the Checkoff::Sections class
 class TestSections < BaseAsana
+  let_mock :project, :inactive_task_b, :a_membership, :a_membership_project, :a_membership_section
+
   def expect_named(task, name)
     task.expects(:name).returns(name).at_least(1)
   end
@@ -26,21 +28,39 @@ class TestSections < BaseAsana
       .at_least(1)
   end
 
-  def expect_tasks_and_sections_pulled(workspace, project, project_name)
+  def expect_project_pulled(workspace, project, project_name)
     @mocks[:projects]
       .expects(:project).with(workspace, project_name)
       .returns(project)
       .at_least(1)
+  end
+
+  def expect_task_project_memberships_queried
+    a_membership.expects(:[]).with('project').returns(a_membership_project)
+    a_membership_project.expects(:[]).with('gid').returns(a_gid)
+  end
+
+  def expect_task_section_memberships_queried
+    a_membership.expects(:[]).with('section').returns(a_membership_section)
+    a_membership_section.expects(:[]).with('name').returns('Section 1:')
+  end
+
+  def expect_task_memberships_queried
+    task_c.expects(:memberships).returns([a_membership])
+    expect_task_project_memberships_queried
+    expect_task_section_memberships_queried
+  end
+
+  def expect_tasks_and_sections_pulled(workspace, project, project_name)
+    expect_project_pulled(workspace, project, project_name)
     expect_tasks_pulled(project, [task_a, task_b, section_1, task_c],
-                        [section_1, task_c])
-    expect_section_1_queried
-    expect_named(task_c, 'task c')
+                        [task_c])
+    expect_task_memberships_queried
   end
 
   def expect_project_a_tasks_pulled
     expect_tasks_and_sections_pulled('Workspace 1', project_a, a_name)
     project_a.expects(:gid).returns(a_gid)
-    section_1.expects(:memberships).returns([{}])
   end
 
   def mock_section_pulled
@@ -53,7 +73,15 @@ class TestSections < BaseAsana
     expect_tasks_and_sections_pulled('My Workspace', my_tasks_in_workspace,
                                      project_sym)
     section_1.expects(:assignee_status).returns(assignee_status)
-    task_c.expects(:assignee_status).returns(assignee_status)
+    tapsk_c.expects(:assignee_status).returns(assignee_status)
+  end
+
+  def test_tasks_normal_project
+    asana = get_test_object do
+      mock_section_pulled
+    end
+    out = asana.tasks('Workspace 1', a_name, 'Section 1:')
+    assert_equal([task_c], out)
   end
 
   def test_tasks_project_not_found
