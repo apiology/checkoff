4c99349df2a11975f267cbed047922345eac62db
Vince Broz <vince@broz.cc>
2021-06-04 08:48:40 -0400

    Mark private methods in cli.rb

--- lib/checkoff/cli.rb
+++ lib/checkoff/cli.rb
@@ -32,59 +32,41 @@ module Checkoff
       @stderr = stderr
     end
 
-    def task_to_hash(task)
-      task_out = {
-        name: task.name,
-      }
-      if task.due_on
-        task_out[:due] = task.due_on
-      elsif task.due_at
-        task_out[:due] = task.due_at
+    def run(args)
+      validate_args!(args)
+      command, subargs = parse_args(args)
+      case command
+      when 'view'
+        view(subargs.workspace, subargs.project, subargs.section)
+      when 'quickadd'
+        quickadd(subargs.workspace, subargs.task_name)
+      else
+        raise
       end
-      task_out
     end
 
-    def tasks_to_hash(tasks)
-      tasks.map { |task| task_to_hash(task) }
-    end
+    private
 
-    def run_on_project(workspace, project)
-      tasks_by_section =
-        sections.tasks_by_section(workspace, project)
-      tasks_by_section.update(tasks_by_section) do |_key, tasks|
-        tasks_to_hash(tasks)
+    def view(workspace_name, project_name, section_name)
+      if project_name.nil?
+        stderr.puts 'Please specify a project name'
+        exit(1)
       end
-      tasks_by_section.to_json
+      project_name = project_name[1..].to_sym if project_name.start_with? ':'
+      if section_name.nil?
+        run_on_project(workspace_name, project_name)
+      else
+        run_on_section(workspace_name, project_name, section_name)
+      end
     end
 
-    def run_on_section(workspace, project, section)
-      section = nil if section == ''
-      tasks = sections.tasks(workspace, project, section) || []
-      tasks_to_hash(tasks).to_json
-    end
-
-    def quickadd(workspace_name, task_name)
-      workspace = @workspaces.workspace_by_name(workspace_name)
-      @tasks.add_task(task_name,
-                      workspace_gid: workspace.gid)
-    end
-
-    def validate_args!(args)
-      return unless args.length < 2 || !%w[view quickadd].include?(args[0])
-
-      output_help
-      exit(1)
-    end
-
-    def parse_view_args(subargs, args)
-      subargs.workspace = args[1]
-      subargs.project = args[2]
-      subargs.section = args[3]
-    end
-
-    def parse_quickadd_args(subargs, args)
-      subargs.workspace = args[1]
-      subargs.task_name = args[2]
+    def output_help
+      stderr.puts 'View tasks:'
+      stderr.puts "  #{$PROGRAM_NAME} view workspace project [section]"
+      stderr.puts "  #{$PROGRAM_NAME} quickadd workspace task_name"
+      stderr.puts
+      stderr.puts "'project' can be set to a project name, or :my_tasks, " \
+                  ":my_tasks_upcoming, :my_tasks_new, or :my_tasks_today"
     end
 
     # rubocop:disable Metrics/MethodLength
@@ -103,39 +85,59 @@ module Checkoff
     end
     # rubocop:enable Metrics/MethodLength
 
-    def output_help
-      stderr.puts 'View tasks:'
-      stderr.puts "  #{$PROGRAM_NAME} view workspace project [section]"
-      stderr.puts "  #{$PROGRAM_NAME} quickadd workspace task_name"
-      stderr.puts
-      stderr.puts "'project' can be set to a project name, or :my_tasks, " \
-                  ":my_tasks_upcoming, :my_tasks_new, or :my_tasks_today"
+    def parse_quickadd_args(subargs, args)
+      subargs.workspace = args[1]
+      subargs.task_name = args[2]
     end
 
-    def view(workspace_name, project_name, section_name)
-      if project_name.nil?
-        stderr.puts 'Please specify a project name'
-        exit(1)
-      end
-      project_name = project_name[1..].to_sym if project_name.start_with? ':'
-      if section_name.nil?
-        run_on_project(workspace_name, project_name)
-      else
-        run_on_section(workspace_name, project_name, section_name)
-      end
+    def parse_view_args(subargs, args)
+      subargs.workspace = args[1]
+      subargs.project = args[2]
+      subargs.section = args[3]
     end
 
-    def run(args)
-      validate_args!(args)
-      command, subargs = parse_args(args)
-      case command
-      when 'view'
-        view(subargs.workspace, subargs.project, subargs.section)
-      when 'quickadd'
-        quickadd(subargs.workspace, subargs.task_name)
-      else
-        raise
+    def validate_args!(args)
+      return unless args.length < 2 || !%w[view quickadd].include?(args[0])
+
+      output_help
+      exit(1)
+    end
+
+    def quickadd(workspace_name, task_name)
+      workspace = @workspaces.workspace_by_name(workspace_name)
+      @tasks.add_task(task_name,
+                      workspace_gid: workspace.gid)
+    end
+
+    def run_on_section(workspace, project, section)
+      section = nil if section == ''
+      tasks = sections.tasks(workspace, project, section) || []
+      tasks_to_hash(tasks).to_json
+    end
+
+    def run_on_project(workspace, project)
+      tasks_by_section =
+        sections.tasks_by_section(workspace, project)
+      tasks_by_section.update(tasks_by_section) do |_key, tasks|
+        tasks_to_hash(tasks)
       end
+      tasks_by_section.to_json
+    end
+
+    def task_to_hash(task)
+      task_out = {
+        name: task.name,
+      }
+      if task.due_on
+        task_out[:due] = task.due_on
+      elsif task.due_at
+        task_out[:due] = task.due_at
+      end
+      task_out
+    end
+
+    def tasks_to_hash(tasks)
+      tasks.map { |task| task_to_hash(task) }
     end
   end
   # rubocop:enable Metrics/ClassLength
