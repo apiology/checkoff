869c24ba71e13edb5b373f2f6a25d4e826ae9b66
Vince Broz <vince@broz.cc>
2021-08-01 10:57:33 -0400

    Add Subtask#subtask_section?(task) method

--- test/unit/test_subtasks.rb
+++ test/unit/test_subtasks.rb
@@ -8,7 +8,58 @@ class TestSubtasks < ClassTest
 
   def_delegators(:@mocks, :projects)
 
-  let_mock :task, :task_options, :raw_subtasks
+  let_mock :task, :task_options, :raw_subtasks,
+           :subtask,
+           :subtask_section_1, :subtask_1a, :subtask_1b,
+           :subtask_section_2,
+           :subtask_section_3, :subtask_3a
+
+  def test_subtask_section_false
+    subtasks = get_test_object do
+      subtask.expects(:name).returns('foo')
+    end
+    refute(subtasks.subtask_section?(subtask))
+  end
+
+  def test_subtask_section_true
+    subtasks = get_test_object do
+      subtask.expects(:name).returns('foo:')
+    end
+    assert(subtasks.subtask_section?(subtask))
+  end
+
+  def expect_subtask_section_1_named
+    subtask_section_1.expects(:name).returns('1:').at_least(1)
+    subtask_1a.expects(:name).returns('1a')
+    subtask_1b.expects(:name).returns('1b')
+  end
+
+  def expect_subtask_section_2_named
+    subtask_section_2.expects(:name).returns('2:').at_least(1)
+  end
+
+  def expect_subtask_section_3_named
+    subtask_section_3.expects(:name).returns('3:').at_least(1)
+    subtask_3a.expects(:name).returns('3a')
+  end
+
+  def expect_subtask_names_queried
+    expect_subtask_section_1_named
+    expect_subtask_section_2_named
+    expect_subtask_section_3_named
+  end
+
+  def test_by_section
+    subtasks = get_test_object { expect_subtask_names_queried }
+    assert_equal({
+                   '1:' => [subtask_1a, subtask_1b],
+                   '2:' => [],
+                   '3:' => [subtask_3a],
+                 },
+                 subtasks.by_section([subtask_section_1, subtask_1a, subtask_1b,
+                                      subtask_section_2,
+                                      subtask_section_3, subtask_3a]))
+  end
 
   def test_raw_subtasks
     subtasks = get_test_object do
