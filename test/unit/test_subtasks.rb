085a27fdaccd067fda4619503ff08bc10df89cdd
Vince Broz <vince@broz.cc>
2021-08-01 11:23:50 -0400

    Add Subtask#all_subtasks_completed?(task) method
    Demote subtask_section?() to private as it's likely to be unneeded in
    future with Asana moving to first class sections in subtasks
    eventually.

--- test/unit/test_subtasks.rb
+++ test/unit/test_subtasks.rb
@@ -14,43 +14,77 @@ class TestSubtasks < ClassTest
            :subtask_section_2,
            :subtask_section_3, :subtask_3a
 
+  def test_all_subtasks_completed_false
+    subtasks = get_test_object do
+      expect_task_options_pulled
+      expect_raw_subtasks_pulled
+      active_subtasks = [subtask_section_1, subtask_1a, subtask_1b,
+                         subtask_section_2,
+                         subtask_section_3, subtask_3a]
+      expect_active_subtasks_pulled(active_subtasks)
+      allow_subtask_names_queried
+    end
+    refute(subtasks.all_subtasks_completed?(task))
+  end
+
+  def expect_subtask_completed(subtask)
+    subtask.expects(:completed_at).returns('2020-01-01 12:00pm')
+  end
+
+  def expect_active_subtasks_pulled(active_subtasks)
+    projects.expects(:active_tasks).with(raw_subtasks).returns(active_subtasks)
+  end
+
+  def test_all_subtasks_completed_true
+    subtasks = get_test_object do
+      expect_task_options_pulled
+      expect_raw_subtasks_pulled
+      active_subtasks = [subtask_section_1,
+                         subtask_section_2,
+                         subtask_section_3]
+      expect_active_subtasks_pulled(active_subtasks)
+      allow_subtask_names_queried
+    end
+    assert(subtasks.all_subtasks_completed?(task))
+  end
+
   def test_subtask_section_false
     subtasks = get_test_object do
       subtask.expects(:name).returns('foo')
     end
-    refute(subtasks.subtask_section?(subtask))
+    refute(subtasks.send(:subtask_section?, subtask))
   end
 
   def test_subtask_section_true
     subtasks = get_test_object do
       subtask.expects(:name).returns('foo:')
     end
-    assert(subtasks.subtask_section?(subtask))
+    assert(subtasks.send(:subtask_section?, subtask))
   end
 
-  def expect_subtask_section_1_named
-    subtask_section_1.expects(:name).returns('1:').at_least(1)
-    subtask_1a.expects(:name).returns('1a')
-    subtask_1b.expects(:name).returns('1b')
+  def allow_subtask_section_1_named
+    subtask_section_1.expects(:name).returns('1:').at_least(0)
+    subtask_1a.expects(:name).returns('1a').at_least(0)
+    subtask_1b.expects(:name).returns('1b').at_least(0)
   end
 
-  def expect_subtask_section_2_named
-    subtask_section_2.expects(:name).returns('2:').at_least(1)
+  def allow_subtask_section_2_named
+    subtask_section_2.expects(:name).returns('2:').at_least(0)
   end
 
-  def expect_subtask_section_3_named
-    subtask_section_3.expects(:name).returns('3:').at_least(1)
-    subtask_3a.expects(:name).returns('3a')
+  def allow_subtask_section_3_named
+    subtask_section_3.expects(:name).returns('3:').at_least(0)
+    subtask_3a.expects(:name).returns('3a').at_least(0)
   end
 
-  def expect_subtask_names_queried
-    expect_subtask_section_1_named
-    expect_subtask_section_2_named
-    expect_subtask_section_3_named
+  def allow_subtask_names_queried
+    allow_subtask_section_1_named
+    allow_subtask_section_2_named
+    allow_subtask_section_3_named
   end
 
   def test_by_section
-    subtasks = get_test_object { expect_subtask_names_queried }
+    subtasks = get_test_object { allow_subtask_names_queried }
     assert_equal({
                    '1:' => [subtask_1a, subtask_1b],
                    '2:' => [],
@@ -61,10 +95,18 @@ class TestSubtasks < ClassTest
                                       subtask_section_3, subtask_3a]))
   end
 
+  def expect_task_options_pulled
+    projects.expects(:task_options).returns(task_options)
+  end
+
+  def expect_raw_subtasks_pulled
+    task.expects(:subtasks).with(task_options).returns(raw_subtasks)
+  end
+
   def test_raw_subtasks
     subtasks = get_test_object do
-      projects.expects(:task_options).returns(task_options)
-      task.expects(:subtasks).with(task_options).returns(raw_subtasks)
+      expect_task_options_pulled
+      expect_raw_subtasks_pulled
     end
     assert_equal(raw_subtasks, subtasks.raw_subtasks(task))
   end
