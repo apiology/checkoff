dd109faf7c651abbb25ae58faeda977901169956
Vince Broz <vince@broz.cc>
2021-08-17 09:03:24 -0400

    Introduce tags and custom fields as entities

---
+++ lib/checkoff/tags.rb
@@ -0,0 +1,68 @@
+#!/usr/bin/env ruby
+
+# frozen_string_literal: true
+
+require 'forwardable'
+require 'cache_method'
+require_relative 'config_loader'
+require_relative 'workspaces'
+require_relative 'clients'
+
+# https://developers.asana.com/docs/tags
+
+module Checkoff
+  # Work with tags in Asana
+  class Tags
+    MINUTE = 60
+    HOUR = MINUTE * 60
+    DAY = 24 * HOUR
+    REALLY_LONG_CACHE_TIME = HOUR * 1
+    LONG_CACHE_TIME = MINUTE * 15
+    SHORT_CACHE_TIME = MINUTE
+
+    def initialize(config: Checkoff::ConfigLoader.load(:asana),
+                   workspaces: Checkoff::Workspaces.new(config: config),
+                   clients: Checkoff::Clients.new(config: config),
+                   client: clients.client)
+      @workspaces = workspaces
+      @client = client
+    end
+
+    def tag_or_raise(workspace_name, tag_name)
+      tag = tag(workspace_name, tag_name)
+      raise "Could not find tag #{tag_name} under workspace #{workspace_name}." if tag.nil?
+
+      tag
+    end
+    cache_method :tag_or_raise, LONG_CACHE_TIME
+
+    def tag(workspace_name, tag_name)
+      workspace = workspaces.workspace_by_name(workspace_name)
+      tags = client.tags.get_tags_for_workspace(workspace_gid: workspace.gid)
+      tags.find { |tag| tag.name == tag_name }
+    end
+    cache_method :tag, LONG_CACHE_TIME
+
+    private
+
+    attr_reader :workspaces, :client
+
+    # bundle exec ./tags.rb
+    # :nocov:
+    class << self
+      def run
+        workspace_name = ARGV[0] || raise('Please pass workspace name as first argument')
+        tag_name = ARGV[1] || raise('Please pass tag name as second argument')
+        tags = Checkoff::Tags.new
+        tag = tags.tag_or_raise(workspace_name, tag_name)
+        puts "Results: #{tag}"
+      end
+    end
+    # :nocov:
+  end
+end
+
+# :nocov:
+abs_program_name = File.expand_path($PROGRAM_NAME)
+Checkoff::Tags.run if abs_program_name == __FILE__
+# :nocov:
---
+++ test/unit/test_tags.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+require_relative 'test_helper'
+require_relative 'class_test'
+
+class TestTags < ClassTest
+  extend Forwardable
+
+  def_delegators(:@mocks, :workspaces, :client)
+
+  let_mock :workspace_name, :tag_name, :tag, :workspace, :workspace_gid,
+           :tags_api, :wrong_tag, :wrong_tag_name
+
+  def test_tag_or_raise_raises
+    tags = get_test_object do
+      tag_arr = [wrong_tag]
+      expect_tags_pulled(tag_arr)
+    end
+    assert_raises(RuntimeError) do
+      tags.tag_or_raise(workspace_name, tag_name)
+    end
+  end
+
+  def test_tag_or_raise
+    tags = get_test_object do
+      tag_arr = [wrong_tag, tag]
+      expect_tags_pulled(tag_arr)
+    end
+    assert_equal(tag, tags.tag_or_raise(workspace_name, tag_name))
+  end
+
+  def expect_workspace_pulled
+    workspaces.expects(:workspace_by_name).with(workspace_name).returns(workspace)
+    workspace.expects(:gid).returns(workspace_gid)
+  end
+
+  def allow_tags_named
+    wrong_tag.expects(:name).returns(wrong_tag_name).at_least(0)
+    tag.expects(:name).returns(tag_name).at_least(0)
+  end
+
+  def expect_tags_pulled(tag_arr)
+    expect_workspace_pulled
+    client.expects(:tags).returns(tags_api)
+    tags_api.expects(:get_tags_for_workspace).returns(tag_arr)
+    allow_tags_named
+  end
+
+  def test_tag
+    tags = get_test_object do
+      tag_arr = [wrong_tag, tag]
+      expect_tags_pulled(tag_arr)
+    end
+    assert_equal(tag, tags.tag(workspace_name, tag_name))
+  end
+
+  def class_under_test
+    Checkoff::Tags
+  end
+end
