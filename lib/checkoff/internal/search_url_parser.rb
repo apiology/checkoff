d9c49b52b59395fd8aabb47162b9631286f1f2f8
Vince Broz <vince@broz.cc>
2022-07-10 12:53:37 -0400

    Get first test to pass

--- lib/checkoff/internal/search_url_parser.rb
+++ lib/checkoff/internal/search_url_parser.rb
@@ -2,17 +2,88 @@
 
 # frozen_string_literal: true
 
+require 'active_support/core_ext/hash/except'
+
 module Checkoff
   module Internal
+    # Parse Asana search URLs into parameters suitable to pass into
+    # the /workspaces/{workspace_gid}/tasks/search endpoint
     class SearchUrlParser
       def initialize(_deps = {}); end
 
-      def convert_args(url)
+      def convert_params(url)
         url_params = CGI.parse(URI.parse(url).query)
-        url_params.to_a.map { |key, values| convert_arg(key, values) }.to_h
+        custom_field_params, regular_url_params = partition_url_params(url_params)
+        custom_field_args = convert_custom_field_params(custom_field_params)
+        regular_url_args = convert_regular_params(regular_url_params)
+        custom_field_args.merge(regular_url_args)
       end
 
       private
+
+      def convert_regular_params(regular_url_params)
+        regular_url_params.to_a.map do |key, values|
+          convert_arg(key, values)
+        end.to_h
+      end
+
+      def convert_custom_field_params(custom_field_params)
+        by_custom_field = custom_field_params.group_by do |key, _value|
+          gid_from_custom_field_key(key)
+        end.transform_values(&:to_h)
+        by_custom_field.map do |gid, single_custom_field_params|
+          convert_single_custom_field_params(gid, single_custom_field_params)
+        end.inject(&:merge).to_h
+      end
+
+      def convert_single_custom_field_is_params(remaining_params)
+        unless remaining_params.length == 1
+          raise "Teach me how to handle these remaining keys for #{variant_key}: #{remaining_params}"
+        end
+
+        key, values = remaining_params.to_a[0]
+        convert_custom_field_is_arg(key, values)
+      end
+
+      def convert_single_custom_field_no_value_params(remaining_params)
+        unless remaining_params.length.zero?
+          raise "Teach me how to handle these remaining keys for #{variant_key}: #{remaining_params}"
+        end
+
+        { 'custom_fields.1234.is_set' => 'false' }
+      end
+
+      def convert_single_custom_field_params(gid, single_custom_field_params)
+        variant_key = "custom_field_#{gid}.variant"
+        variant = single_custom_field_params.fetch(variant_key)
+        remaining_params = single_custom_field_params.except(variant_key)
+        case variant
+        when ['is']
+          return convert_single_custom_field_is_params(remaining_params)
+        when ['no_value']
+          return convert_single_custom_field_no_value_params(remaining_params)
+        end
+
+        raise "Teach me how to handle #{variant_key} = #{variant}"
+      end
+
+      def convert_custom_field_is_arg(key, values)
+        gid = gid_from_custom_field_key(key)
+
+        if key.end_with? '.selected_options'
+          raise "Too many values found for #{key}: #{values}" if values.length != 1
+
+          return { "custom_fields.#{gid}.value" => values[0] }
+        end
+
+        raise "Teach me how to handle #{key} = #{values}"
+      end
+
+      def partition_url_params(url_params)
+        url_params.to_a.partition do |key, _values|
+          key.start_with? 'custom_field_'
+        end.map(&:to_h)
+      end
 
       PARAM_MAP = {
         'any_projects.ids' => 'projects.any',
@@ -20,33 +91,6 @@ module Checkoff
 
       def gid_from_custom_field_key(key)
         key.split('_')[2].split('.')[0]
-      end
-
-      def convert_custom_field_variant_arg(gid, key, values)
-        case values
-        when ['no_value']
-          # appears when you search for a text or numeric field 'With no value'
-          return ["custom_fields.#{gid}.is_set", 'false']
-        when ['is']
-          return ["custom_fields.#{gid}.is_set", 'true']
-#        when ['is_not']
-#          # appears when you search for an enum field 'With no value'
-        end
-
-        raise "Teach me how to handle #{key} = #{values}"
-      end
-
-      def convert_custom_field_arg(key, values)
-        gid = gid_from_custom_field_key(key)
-        return convert_custom_field_variant_arg(gid, key, values) if key.end_with? '.variant'
-
-        if key.end_with? '.selected_options'
-          raise "Too many values found for #{key}: #{values}" if values.length != 1
-
-          return ["custom_fields.#{gid}.value", values[0]]
-        end
-
-        raise "Teach me how to handle #{key} = #{values}"
       end
 
       def convert_arg(key, values)
--- test/unit/internal/test_search_url_parser.rb
+++ test/unit/internal/test_search_url_parser.rb
@@ -5,7 +5,7 @@ require_relative '../class_test'
 require 'checkoff/internal/search_url_parser'
 
 class TestSearchUrlParser < ClassTest
-  def test_convert_args_1
+  def test_convert_params_1
     search_url_parser = get_test_object
     url = 'https://app.asana.com/0/search?any_projects.ids=123&custom_field_456.variant=is&custom_field_456.selected_options=789&custom_field_1234.variant=no_value'
     asana_api_params = {
@@ -13,10 +13,10 @@ class TestSearchUrlParser < ClassTest
       'custom_fields.456.value' => '789',
       'custom_fields.1234.is_set' => 'false',
     }
-    assert_equal(asana_api_params, search_url_parser.convert_args(url))
+    assert_equal(asana_api_params, search_url_parser.convert_params(url))
   end
 
-  def test_convert_args_2
+  def test_convert_params_2
     search_url_parser = get_test_object
     url = 'https://app.asana.com/0/search?any_projects.ids=123&custom_field_456.variant=no_value&custom_field_789.variant=is&custom_field_789.selected_options=1234'
     asana_api_params = {
@@ -24,10 +24,10 @@ class TestSearchUrlParser < ClassTest
       'custom_fields.456.is_set' => 'false',
       'custom_fields.789.value' => '1234',
     }
-    assert_equal(asana_api_params, search_url_parser.convert_args(url))
+    assert_equal(asana_api_params, search_url_parser.convert_params(url))
   end
 
-  def test_convert_args_3
+  def test_convert_params_3
     search_url_parser = get_test_object
     # TODO: custom_field_5678.variant=is_not is not available in API
     # that I can see...
@@ -37,7 +37,7 @@ class TestSearchUrlParser < ClassTest
       'custom_fields.456.is_set' => 'false',
       'custom_fields.789.value' => '1234',
     }
-    assert_equal(asana_api_params, search_url_parser.convert_args(url))
+    assert_equal(asana_api_params, search_url_parser.convert_params(url))
   end
 
   def class_under_test
