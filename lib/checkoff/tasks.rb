43ba17684c0406e49850b1e02dbd6af0f20e8131
Vince Broz <vince@broz.cc>
2023-10-20 07:37:59 -0400

    Document tasks#task_ready?

--- lib/checkoff/tasks.rb
+++ lib/checkoff/tasks.rb
@@ -42,6 +42,16 @@ module Checkoff
       @workspaces = workspaces
     end
 
+    # Indicates a task is ready for a person to work on it.  This is
+    # subtly different than what is used by Asana to mark a date as
+    # red/green!  A task is ready if it is not dependent on an
+    # incomplete task and one of these is true:
+    #
+    # * start is null and due on is today
+    # * start is null and due at is after now
+    # * start on is today
+    # * start at is after now
+    #
     # @param task [Asana::Resources::Task]
     # @param ignore_dependencies [Boolean]
     def task_ready?(task, ignore_dependencies: false)
