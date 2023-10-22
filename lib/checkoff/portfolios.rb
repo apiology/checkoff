c3c1bfe92a748eaf126db0700f5bead19ebc5483
Vince Broz <vince@broz.cc>
2023-10-22 17:26:23 -0400

    Add Checkoff::Portfolios class

---
+++ lib/checkoff/portfolios.rb
@@ -0,0 +1,112 @@
+#!/usr/bin/env ruby
+
+# frozen_string_literal: true
+
+require 'forwardable'
+require 'cache_method'
+require_relative 'internal/config_loader'
+require_relative 'workspaces'
+require_relative 'clients'
+
+# https://developers.asana.com/reference/portfolios
+
+module Checkoff
+  # Pull portfolios from Asana
+  class Portfolios
+    # @!parse
+    #   extend CacheMethod::ClassMethods
+
+    MINUTE = 60
+    HOUR = MINUTE * 60
+    DAY = 24 * HOUR
+    REALLY_LONG_CACHE_TIME = HOUR * 1
+    LONG_CACHE_TIME = MINUTE * 15
+    SHORT_CACHE_TIME = MINUTE
+
+    # @param config [Hash]
+    # @param workspaces [Checkoff::Workspaces]
+    # @param clients [Checkoff::Clients]
+    # @param client [Asana::Client]
+    def initialize(config: Checkoff::Internal::ConfigLoader.load(:asana),
+                   workspaces: Checkoff::Workspaces.new(config: config),
+                   clients: Checkoff::Clients.new(config: config),
+                   client: clients.client)
+      @workspaces = workspaces
+      @client = client
+    end
+
+    # @param workspace_name [String]
+    # @param portfolio_name [String]
+    #
+    # @return [Asana::Resources::Portfolio]
+    def portfolio_or_raise(workspace_name, portfolio_name)
+      portfolio_obj = portfolio(workspace_name, portfolio_name)
+      raise "Could not find portfolio #{portfolio_name} under workspace #{workspace_name}." if portfolio_obj.nil?
+
+      portfolio_obj
+    end
+    cache_method :portfolio_or_raise, LONG_CACHE_TIME
+
+    # @param workspace_name [String]
+    # @param portfolio_name [String]
+    #
+    # @sg-ignore
+    # @return [Asana::Resources::Portfolio,nil]
+    def portfolio(workspace_name, portfolio_name)
+      workspace = workspaces.workspace_or_raise(workspace_name)
+      me = client.users.me
+      portfolio_objs = client.portfolios.find_all(workspace: workspace.gid,
+                                                  owner: me.gid)
+      # @type [Asana::Resources::Portfolio, nil]
+      portfolio_objs.find { |portfolio_obj| portfolio_obj.name == portfolio_name }
+    end
+    cache_method :portfolio, LONG_CACHE_TIME
+
+    # Pull a specific portfolio by gid
+    #
+    # @param portfolio_gid [String]
+    # @param extra_fields [Array<String>]
+    #
+    # @return [Asana::Resources::Portfolio, nil]
+    def portfolio_by_gid(portfolio_gid,
+                         extra_fields: [])
+      options = {
+        fields: ['name'],
+      }
+      options[:fields] += extra_fields
+      client.portfolios.find_by_id(portfolio_gid, options: options)
+    end
+    cache_method :portfolio_by_gid, SHORT_CACHE_TIME
+
+    private
+
+    # @return [Checkoff::Workspaces]
+    attr_reader :workspaces
+
+    # @return [Asana::Client]
+    attr_reader :client
+
+    # bundle exec ./portfolios.rb
+    # :nocov:
+    class << self
+      # @return [void]
+      def run
+        # @sg-ignore
+        # @type [String]
+        workspace_name = ARGV[0] || raise('Please pass workspace name as first argument')
+        # @sg-ignore
+        # @type [String]
+        portfolio_name = ARGV[1] || raise('Please pass portfolio name as second argument')
+        portfolios = Checkoff::Portfolios.new
+        portfolio = portfolios.portfolio_or_raise(workspace_name, portfolio_name)
+        puts "Results: #{portfolio}"
+      end
+    end
+    # :nocov:
+  end
+end
+
+# :nocov:
+abs_program_name = File.expand_path($PROGRAM_NAME)
+Checkoff::Portfolios.run if abs_program_name == File.expand_path(__FILE__)
+# :nocov:
---
+++ test/unit/test_portfolios.rb
@@ -0,0 +1,96 @@
+# frozen_string_literal: true
+
+require_relative 'test_helper'
+require_relative 'class_test'
+require 'checkoff/portfolios'
+
+class TestPortfolios < ClassTest
+  extend Forwardable
+
+  def_delegators(:@mocks, :workspaces, :client)
+
+  let_mock :workspace_name, :portfolio_name, :portfolio, :workspace, :workspace_gid,
+           :portfolios_api, :wrong_portfolio, :wrong_portfolio_name, :users_api, :me, :me_gid,
+           :portfolio_gid
+
+  def test_portfolio_or_raise_raises
+    portfolios = get_test_object do
+      portfolio_arr = [wrong_portfolio]
+      expect_portfolios_pulled(portfolio_arr)
+    end
+    assert_raises(RuntimeError) do
+      portfolios.portfolio_or_raise(workspace_name, portfolio_name)
+    end
+  end
+
+  def test_portfolio_or_raise
+    portfolios = get_test_object do
+      portfolio_arr = [wrong_portfolio, portfolio]
+      expect_portfolios_pulled(portfolio_arr)
+    end
+    assert_equal(portfolio, portfolios.portfolio_or_raise(workspace_name, portfolio_name))
+  end
+
+  def expect_workspace_pulled
+    workspaces.expects(:workspace_or_raise).with(workspace_name).returns(workspace)
+    workspace.expects(:gid).returns(workspace_gid)
+  end
+
+  def allow_portfolios_named
+    wrong_portfolio.expects(:name).returns(wrong_portfolio_name).at_least(0)
+    portfolio.expects(:name).returns(portfolio_name).at_least(0)
+  end
+
+  def expect_portfolios_api_pulled
+    client.expects(:portfolios).returns(portfolios_api)
+  end
+
+  def expect_me_gid_pulled
+    client.expects(:users).returns(users_api)
+    users_api.expects(:me).returns(me)
+    me.expects(:gid).returns(me_gid)
+  end
+
+  def expect_portfolios_pulled(portfolio_arr)
+    expect_workspace_pulled
+    expect_portfolios_api_pulled
+    expect_me_gid_pulled
+    portfolios_api.expects(:find_all).with(workspace: workspace_gid,
+                                           owner: me_gid).returns(portfolio_arr)
+    allow_portfolios_named
+  end
+
+  def test_portfolio
+    portfolios = get_test_object do
+      portfolio_arr = [wrong_portfolio, portfolio]
+      expect_portfolios_pulled(portfolio_arr)
+    end
+    assert_equal(portfolio, portfolios.portfolio(workspace_name, portfolio_name))
+  end
+
+  def test_portfolio_by_gid
+    portfolios = get_test_object do
+      expect_portfolios_api_pulled
+      portfolios_api.expects(:find_by_id).with(portfolio_gid,
+                                               options: { fields: ['name'] }).returns(portfolio)
+    end
+    assert_equal(portfolio, portfolios.portfolio_by_gid(portfolio_gid))
+  end
+
+  def class_under_test
+    Checkoff::Portfolios
+  end
+
+  def respond_like_instance_of
+    {
+      config: Hash,
+      workspaces: Checkoff::Workspaces,
+      clients: Checkoff::Clients,
+      client: Asana::Client,
+    }
+  end
+
+  def respond_like
+    {}
+  end
+end
