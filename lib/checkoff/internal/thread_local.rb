f9c0ba90babb439de93f4d8396579a3834a5825b
Vince Broz <vince@broz.cc>
2024-01-15 15:07:55 -0500

    Annotate task name lookup for webhook registration

---
+++ lib/checkoff/internal/thread_local.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Checkoff
+  module Internal
+    # Manage thread lock variables in a block
+    class ThreadLocal
+      # @sg-ignore
+      # @param name [Symbol]
+      # @param value [Object,Boolean]
+      #
+      # @return [Object,Boolean]
+      def with_thread_local_variable(name, value, &block)
+        old_value = Thread.current[name]
+        Thread.current[name] = value
+        block.yield
+      ensure
+        Thread.current[name] = old_value
+      end
+    end
+  end
+end
