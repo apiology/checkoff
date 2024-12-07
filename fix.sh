ba19aeb8d914a326bba01c183ddec2af2587a3f2
Vince Broz <vince@broz.cc>
2024-12-07 15:31:07 -0500

    Debug

--- fix.sh
+++ fix.sh
@@ -485,7 +485,7 @@ ensure_overcommit() {
 
 ensure_rugged_packages_installed() {
   # only needed if we don't already have rugged installed
-  if ! ls vendor/bundle/gems/rugged-*
+  if ! ls vendor/bundle/ruby/*/gems/rugged-*
   then
     echo "Current directory"
     pwd
